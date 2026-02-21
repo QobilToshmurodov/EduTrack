8c80009af456da308215a4edb5fc6d0f83539d2b
golibjon94 <golib94jon@gmail.com>
2026-02-21 12:23:45 +0500

    Added migration for submission descriptions and updated seed data.
    - Created `AddSubmissionDescription` migration to include the `Description` column in the `Submissions` table.
    - Updated `PasswordHash` for the default admin user in the database seed data.
    - Added initial files for uploaded submissions and assignments.

---
+++ EduTrackDataAccess/Migrations/20260221064034_AddSubmissionDescription.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduTrackDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubmissionDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Submissions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$Vm3DbG.hL7FM.dmN1T//EuOKRyn5LXucMaIyGVAmDtQf94ED76RES");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Submissions");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$ysMyeog0DjDtJA8XfNjC.uN42CUJ2O.9NvTi/rngnMawwOwRfiu6W");
+        }
+    }
+}
